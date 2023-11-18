c5c9f9ae47a74448f69d2c6bb27537105cd69fa0
Max Castro Vidal <maxcv@hotmail.com>
2023-11-18 17:45:23 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+namespace CurrentTimeService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+
+            app.UseHttpsRedirection();
+
+
+            app.MapGet("/time/uct", () => Results.Ok(DateTime.UtcNow));
+
+            app.Run();
+        }
+    }
+}
